85fa79776308fba14274cca6c8507f90030908f3
root <root@568372f92592>
2021-07-14 23:48:33 +0000

    bug

--- src/_06_gridworld/Bug.java
+++ src/_06_gridworld/Bug.java
@@ -18,14 +18,45 @@ public static void main(String[] args) {
 	Location otherLoc = new Location(5,6);
 	acorn.add(otherLoc, barry);
 	Bug binco = new Bug();
+	Bug bob = new Bug();
+	Flower frigaDoor = new Flower();
+	Flower fritata = new Flower();
+	Bug boots = new Bug();
+	Flower fungi = new Flower();
+	Flower fier = new Flower();
+	Flower far = new Flower();
+
+
 	
 	Location popcorn = new Location(7, 8);
 	acorn.add(popcorn, binco);
-
 	
+	//8,5
+Location popcornsFriend = new Location(8,5);
+	acorn.add(popcornsFriend,bob );
  
-	
+Location popcornsFriendsFriend = new Location(5,7);	
+acorn.add(popcornsFriendsFriend, frigaDoor);
+
+Location popcorntv = new Location(6,8);
+acorn.add(popcorntv, fritata);
+
+Location popcornnet = new Location (3,7);
+acorn.add(popcornnet, boots);
+
+Location popcornsComputer = new Location(5,4);
+acorn.add(popcornsComputer, fungi);
+
+//4,3
+Location popcornsHouse = new Location(4,3);
+acorn.add(popcornsHouse, fier);
+
+//7,5
+Location popcornstree = new Location(3,5);
+acorn.add(popcornstree, fier);
+
 }
+
 
 
 
